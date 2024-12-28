572deab229e8f646b42f60d0fd54621be1c45996
mittalas <mittalas@amazon.com>
2024-12-28 12:35:02 -0800

    Inital commit

---
+++ src/main/java/com/example/javacodingjourney/JavaCodingJourneyApplication.java
@@ -0,0 +1,13 @@
+package com.example.javacodingjourney;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaCodingJourneyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaCodingJourneyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/javacodingjourney/JavaCodingJourneyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javacodingjourney;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaCodingJourneyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
