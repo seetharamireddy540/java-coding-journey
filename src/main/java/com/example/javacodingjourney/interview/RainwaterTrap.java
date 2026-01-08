84bee0469e78cf12b060d48a966f87bc473dd583
Ram Mittala <mittalas@amazon.com>
2026-01-08 08:12:12 -0800

    Solution new problem

--- src/main/java/com/example/javacodingjourney/interview/RainwaterTrap.java
+++ src/main/java/com/example/javacodingjourney/interview/RainwaterTrap.java
@@ -2,6 +2,12 @@ package com.example.javacodingjourney.interview;
 
 public class RainwaterTrap {
 
+    public static void main(String[] args) {
+
+        System.out.println(new RainwaterTrap().trap(new int[]{0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1}));
+        System.out.println(new RainwaterTrap().trap(new int[]{4,2,0,3,2,5}));
+    }
+
     public int trap(int[] height) {
         int left = 0, right = height.length - 1;
         int leftMax = 0, rightMax = 0;
